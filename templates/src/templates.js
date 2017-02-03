9233ff5f352dad21fe150f15335bccab2ce7174d
mdogadailo <m.dogadailo@gmail.com>
2017-02-03 12:10:14 +0100

    jslint

--- templates/src/templates.js
+++ templates/src/templates.js
@@ -4,20 +4,19 @@
  * @param {Object} dependence - list of dependencies to replace.
  * @param {string} left_separator - left separator.
  * @param {string} right_separator right separator.
- * @param {bool} strict - replacement mode (true - don't replace missed dependence, false - replace with empty value) 
+ * @param {bool} strict - replacement mode (true - don't replace missed dependence, false - replace with empty value)
  * @return {string} - converted string.
  */
-template = function(template, dependence, left_separator, right_separator, strict){
+template = function (template, dependence, left_separator, right_separator, strict) {
     // Exit if not a string
-    if(!template) return '';
+    if (!template) return '';
 
-    var dependence = (dependence||[]),
-        left_separator = (left_separator||'%%'),
-        right_separator = (right_separator||'%%');
-        
-    return template.replace(new RegExp(left_separator+"(.*?)"+right_separator,"ig"), function(def, key){
+    var dependence = (dependence || []),
+        left_separator = (left_separator || '%%'),
+        right_separator = (right_separator || '%%');
+
+    return template.replace(new RegExp(left_separator + "(.*?)" + right_separator, "ig"), function (def, key) {
         // replace by dependence list
         return dependence[key] ? dependence[key] : strict ? def : ''
     });
 }
-
